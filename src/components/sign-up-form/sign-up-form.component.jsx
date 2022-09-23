c85d62f1873138790e05f49b9e42fc338ea19dec
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-09-23 16:47:01 +0800

    Subscribe listeners to auth object

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -3,7 +3,6 @@ import { createAuthUserWithEmailandPassword, createUserDocumentFromAuth } from '
 import FormInput from '../form-input/form-input.component';
 import './sign-up-form.styles.scss';
 import Button from '../button/button.component';
-
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -14,7 +13,6 @@ const defaultFormFields = {
 const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
-
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormFields({...formFields, [name]: value })
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,23 +1,32 @@
 import { Outlet, Link } from 'react-router-dom';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
+import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 const Navigation = () => {
+    const { currentUser } = useContext(UserContext);
+    
     return (
       <Fragment>
         <div className='navigation'>
-        <Link className='logo-container' to='/'>
-            <CrwnLogo className='logo' />
-        </Link>
-        <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop'>
-                Go to shop
+            <Link className='logo-container' to='/'>
+                <CrwnLogo className='logo' />
             </Link>
-            <Link className='nav-link' to='/auth'>
-                Sign In
-            </Link>
-        </div>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    Go to shop
+                </Link>
+                {
+                    currentUser ? <span className='nav-link' onClick={signOutUser}>Sign Out</span> 
+                    :
+                    <Link className='nav-link' to='/auth'>
+                    Sign In
+                    </Link>
+                }
+
+            </div>
         </div>
         <Outlet />
       </Fragment>

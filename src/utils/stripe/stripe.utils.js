a470482e54c59676d6057bf62fd2457f0e163488
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-10-02 15:58:08 +0800

    Fix log

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,6 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-console.log('Key: ', process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 export const stripePromise = loadStripe(
     process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
